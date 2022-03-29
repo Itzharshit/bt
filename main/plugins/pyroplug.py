ece2a04edb4d9e6df8bcde2e742f7551e98e08af
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-03-29 18:36:00 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -56,7 +56,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i):
                     await client.send_message(sender, msg.text.markdown)
                     await edit.delete()
                     return
-            edit = await client.edit_message_text(sender, edit_id, "Trying to download!)
+            edit = await client.edit_message_text(sender, edit_id, "Trying to download!")
             file = await userbot.download_media(
                 msg,
                 progress=progress_for_pyrogram,
